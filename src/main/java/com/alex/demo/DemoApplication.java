c78eb21093bd86b98056b7ec13270ca696e9c386
Alex Cazares <dextra2022@outlook.com>
2022-03-17 17:50:44 -0600

    Enable api readu to work with Angular

--- src/main/java/com/alex/demo/DemoApplication.java
+++ src/main/java/com/alex/demo/DemoApplication.java
@@ -3,14 +3,32 @@ package com.alex.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class DemoApplication {
+@Configuration
+public class DemoApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry
+					.addMapping("/**")
+					.allowedOrigins("http://localhost:4200")
+					.allowedMethods("GET","POST","PUT","DELETE")
+					.allowedHeaders("Content-Type")
+				;
+			}
+		};
+	}
 }
